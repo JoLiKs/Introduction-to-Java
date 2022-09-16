4c29ddc03ef41e96382fae1b0cccaaa058e9fe54
Egor Ralovich <vonkad002@gmail.com>
2022-09-16 12:33:21 +0300

    Update LinePrograms.java

--- LinePrograms.java
+++ LinePrograms.java
@@ -1,12 +1,16 @@
 public class LinePrograms
 {
 	public static void main(String[] args) {
-		System.out.println(findZ(5,1,3));
+		System.out.println(secondTask(5,1,3));
 	}
 
 //first task
-  public static int findZ(int a, int b, int c) {
+  public static int firstTask(int a, int b, int c) {
     int z = ((a-3)*b/2)+c;
     return z;
   }
+	//second task
+	public static double secondTask(int a, int b, int c) {
+    double res = (b+Math.sqrt(Math.pow(b,2)+4*a*c))/(2*a)-Math.pow(a,3)*c+Math.pow(b, -2);
+    return res;
 }
